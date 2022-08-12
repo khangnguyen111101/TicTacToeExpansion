f66a0d91cb58d3aa389e6fcd74146a8045068c79
Khang Nguyen <72019979+khangnguyen111101@users.noreply.github.com>
2022-08-12 14:39:02 +0700

    Initial Commit

---
+++ TicTacToeExpansion/TicTacToeExpansionApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeExpansionApp.swift
+//  TicTacToeExpansion
+//
+//  Created by Khang on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeExpansionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
